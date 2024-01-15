be0e165d85b7075bd156a8588dd0ff8c813d1ca7
Joseph Collins <joecollins@Josephs-MBP.home>
2024-01-15 13:51:15 +0000

    build out chat structure

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=2352&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <img
+          src="https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=2352&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
           src="https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=2352&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="user image"
         />
-        <span>John</span>
+        <span>John Smith</span>
         <button>logout</button>
       </div>
     </div>
