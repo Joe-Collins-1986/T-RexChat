bbea002cab6d8d4209580377866ea7c3f81bd343
Joseph Collins <joecollins@Josephs-MBP.home>
2024-01-24 16:00:04 +0000

    add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import "./App.scss";
-
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useContext } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
+import "./App.scss";
+
+// Custom hook for tracking page views
+const PageTracker = ({ children }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const gtag = window.gtag;
+    if (gtag) {
+      gtag("config", "G-XDSM0GGX3V", {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return children;
+};
 
 function App() {
   const { currentUser } = useContext(AuthContext);
-  // console.log(currentUser);
 
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
@@ -22,19 +42,21 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          index
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        ></Route>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="register" element={<Register />}></Route>
-      </Routes>
+      <PageTracker>
+        <Routes>
+          <Route
+            path="/"
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="register" element={<Register />}></Route>
+        </Routes>
+      </PageTracker>
     </BrowserRouter>
   );
 }
